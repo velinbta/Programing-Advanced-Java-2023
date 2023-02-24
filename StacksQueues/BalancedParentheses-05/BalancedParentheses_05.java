2d29ece0b83dcaf190576fe2851c8b579d925ea1
velinbta <122949696+velinbta@users.noreply.github.com>
2023-02-24 03:20:16 +0200

    Solution added

---
+++ StacksQueues/BalancedParentheses-05/BalancedParentheses_05.java
@@ -0,0 +1,95 @@
+package StacksQueues;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class BalancedParentheses_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char[] parenthesesArr = scanner.nextLine().toCharArray();
+
+        Deque<Integer> typeFirstStack = new ArrayDeque<>(); // <- Първи вид скоби '{' '}'
+        Deque<Integer> typeSecondStack = new ArrayDeque<>(); // <- Втори вид скоби '(' ')'
+        Deque<Integer> typeThirdStack = new ArrayDeque<>(); // <- Трети вид скоби '[' ']'
+
+        boolean isBalanced = true;
+
+        for (int position = 0; position < parenthesesArr.length; position++) {
+
+            char symbol = parenthesesArr[position]; // <- Текущ символ
+
+            if (symbol == '{') {
+
+                typeFirstStack.push(position);
+
+            } else if (symbol == '}') { // <- Когато намеря затваряща '}'
+
+                if (typeFirstStack.peek() != null) {
+
+                    int result = position - typeFirstStack.pop();
+                    if (result % 2 == 0) { // <- Ако получения резултат има остатък, не са "балансирани"
+                        isBalanced = false;
+                        break;
+                    }
+
+                } else { // <- Ако няма отваряща скоба
+
+                    isBalanced = false;
+
+                }
+
+            } else if (symbol == '(') {
+
+                typeSecondStack.push(position);
+
+            } else if (symbol == ')') { // <- Когато намеря затваряща ')'
+
+                if (typeSecondStack.peek() != null) {
+
+                    int result = position - typeSecondStack.pop();
+                    if (result % 2 == 0) { // <- Ако получения резултат има остатък, не са "балансирани"
+                        isBalanced = false;
+                        break;
+                    }
+
+                } else { // <- Ако няма отваряща скоба
+
+                    isBalanced = false;
+
+                }
+
+
+            } else if (symbol == '[') {
+
+                typeThirdStack.push(position);
+
+            } else if (symbol == ']') { // <- Когато намеря затваряща ']'
+
+                if (typeThirdStack.peek() != null) {
+
+                    int result = position - typeThirdStack.pop();
+                    if (result % 2 == 0) { // <- Ако получения резултат има остатък, не са "балансирани"
+                        isBalanced = false;
+                        break;
+                    }
+
+                } else { // <- Ако няма отваряща скоба
+
+                    isBalanced = false;
+
+                }
+
+            }
+
+        }
+
+        String printResult = isBalanced
+                ? "YES" : "NO";
+
+        System.out.println(printResult);
+
+    }
+
+}
---
+++ StacksQueues/BalancedParentheses-05/README.txt
@@ -0,0 +1,33 @@
+5. Balanced Parentheses
+Given a sequence consisting of parentheses, determine whether the expression is balanced. A sequence of parentheses is balanced if every open parenthesis can be paired uniquely with a closing parenthesis that occurs after the former. Also, the interval between them must be balanced.
+You will be given three types of parentheses: (, {, and [.
+{[()]} - This is a balanced parenthesis.
+{[(])} - This is not a balanced parenthesis.
+
+Input
+Each input consists of a single line, the sequence of parentheses.
+1 <= Length of sequence <= 1000.
+Each character of the sequence will be one of the following: {, }, (, ), [, ].
+
+Output
+For each test case, print on a new line "YES" if the parentheses are balanced. Otherwise, print "NO".
+
+Examples:
+
+Input 1:
+{[()]}
+
+Output 1:
+YES
+
+Input 2:
+{[(])}
+
+Output 2:
+NO
+
+Input 3:
+{{[[(())]]}}
+
+Output 3:
+YES
