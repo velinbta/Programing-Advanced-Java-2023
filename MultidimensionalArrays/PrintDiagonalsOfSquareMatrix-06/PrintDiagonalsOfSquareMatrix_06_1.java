e5fced4d48c1becb09e4a578b8b23a7f43b614d1
velinbta <122949696+velinbta@users.noreply.github.com>
2023-03-06 05:07:54 +0200

    Solution added

---
+++ MultidimensionalArrays/PrintDiagonalsOfSquareMatrix-06/PrintDiagonalsOfSquareMatrix_06_1.java
@@ -0,0 +1,47 @@
+package MultidimensionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrintDiagonalsOfSquareMatrix_06_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int squareMatrixRows = Integer.parseInt(scanner.nextLine()); // <- Броя на масивите в матрицата
+
+        int[][] squareMatrix = fillInSquareMatrix(scanner, squareMatrixRows); // <- Винаги квадрат
+
+        int[][] subMatrix = new int[2][squareMatrixRows]; // <- Подматрица с диагоналните стойности
+
+        for (int row = 0; row < squareMatrix.length; row++) { // <- От ляво надясно диагонал
+
+            int element = squareMatrix[row][row];
+            subMatrix[0][row] = element;
+
+        }
+
+        int frontPosition = 0;
+        for (int row = squareMatrix.length - 1; row >= 0; row--, frontPosition++) { // <- От долу нагоре диагонал
+
+            int element = squareMatrix[row][frontPosition];
+            subMatrix[1][frontPosition] = element;
+
+        }
+
+        // Принтирам подматрицата с диагоналните стойности
+        Arrays.stream(subMatrix).forEach(arr ->
+                System.out.println(Arrays.toString(arr).replaceAll("[\\[\\],]", "")));
+
+    }
+
+    private static int[][] fillInSquareMatrix(Scanner scanner, int rows) {
+        // Протича квадратна матрица
+        int[][] squareMatrix = new int[rows][rows];
+        for (int i = 0; i < squareMatrix.length; i++)
+            squareMatrix[i] = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(e -> Integer.parseInt(e)).toArray();
+
+        return squareMatrix;
+    }
+
+}
