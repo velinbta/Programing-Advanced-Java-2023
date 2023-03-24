2c2f2b3c0f09929fa71b199c93699a39185e2e18
velinbta <122949696+velinbta@users.noreply.github.com>
2023-03-24 07:02:42 +0200

    Solution added

--- SetsMaps/LogsAggregator-10/LogsAggregator_10_1.java
+++ SetsMaps/LogsAggregator-10/LogsAggregator_10_1.java
@@ -2,7 +2,7 @@ package SetsMaps;
 
 import java.util.*;
 
-public class LogsAggregator_10 {
+public class LogsAggregator_10_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,7 +11,7 @@ public class LogsAggregator_10 {
         Map<String, Integer> usersDurationMap = new TreeMap<>(); // <- Потребители/продължителност
         Map<String, Set<String>> usersIpMap = new TreeMap<>(); // <- Потребители/уникални IP адреси
 
-        while (lines-- > 0) {
+        for (int i = 0; i < lines; i++) {
             // Извеждам информацията
             String[] infoArr = scanner.nextLine().split("\\s+");
             String user = infoArr[1];
@@ -27,14 +27,28 @@ public class LogsAggregator_10 {
             usersIpMap.get(user).add(ip);
 
         }
+        // Форматирам си изходните данни и ги принтирам
+        String[] formattedOutputArr = getUsernameInformationFormatted(usersDurationMap, usersIpMap);
+        Arrays.stream(formattedOutputArr).forEach(user -> System.out.println(user));
 
-        usersDurationMap.entrySet().forEach(user -> {
+    }
 
-            System.out.printf("%s: %d ", user.getKey(), user.getValue());
-            System.out.printf("%s\n", usersIpMap.get(user.getKey()).toString());
+    private static String[] getUsernameInformationFormatted(Map<String, Integer> usersDurationMap,
+                                                            Map<String, Set<String>> usersIpMap) {
+        // Създава масив с форматираните изходни данни
+        String[] formattedArr = new String[usersDurationMap.size()];
 
-        });
+        int position = 0;
+        for (Map.Entry<String, Integer> entry : usersDurationMap.entrySet()) {
 
+            String format = String.format("%s: %d %s",
+                    entry.getKey(), entry.getValue(), usersIpMap.get(entry.getKey()).toString());
+            formattedArr[position] = format;
+
+            position++;
+        }
+
+        return formattedArr;
     }
 
 }
---
+++ SetsMaps/LogsAggregator-10/LogsAggregator_10_2.java
@@ -0,0 +1,40 @@
+package SetsMaps;
+
+import java.util.*;
+
+public class LogsAggregator_10_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int lines = Integer.parseInt(scanner.nextLine());
+
+        Map<String, Integer> usersDurationMap = new TreeMap<>(); // <- Потребители/продължителност
+        Map<String, Set<String>> usersIpMap = new TreeMap<>(); // <- Потребители/уникални IP адреси
+
+        while (lines-- > 0) {
+            // Извеждам информацията
+            String[] infoArr = scanner.nextLine().split("\\s+");
+            String user = infoArr[1];
+            String ip = infoArr[0];
+            int duration = Integer.parseInt(infoArr[2]);
+
+            // Смятам и добавям на момента продължителността за потребителя
+            usersDurationMap.putIfAbsent(user, 0);
+            usersDurationMap.put(user, usersDurationMap.get(user) + duration);
+
+            // Добавям същия потребител с уникални негови IP адреси
+            usersIpMap.putIfAbsent(user, new TreeSet<>());
+            usersIpMap.get(user).add(ip);
+
+        }
+
+        usersDurationMap.entrySet().forEach(user -> {
+
+            System.out.printf("%s: %d ", user.getKey(), user.getValue());
+            System.out.printf("%s\n", usersIpMap.get(user.getKey()).toString());
+
+        });
+
+    }
+
+}
