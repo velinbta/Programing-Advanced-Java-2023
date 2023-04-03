d2f6db41c6271af5802212bb4fb208b51ab0add1
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-03 21:20:39 +0300

    Solution added

--- SetsMaps/PeriodicTable-03/PeriodicTable_03_2.java
+++ SetsMaps/PeriodicTable-03/PeriodicTable_03_2.java
@@ -1,27 +1,24 @@
 package SetsMaps;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class PeriodicTable_03_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int lines = Integer.parseInt(scanner.nextLine());
+        int numberOfCompounds = Integer.parseInt(scanner.nextLine());
 
-        Set<String> chemicalElementsSet = new TreeSet<>(); // <- По азбучен ред
-
-        while (lines-- > 0) {
-
-            String[] currentElementsArr = scanner.nextLine().split("\\s+");
-            // Добавям текущите прочетени елементи
-            Arrays.stream(currentElementsArr).forEach(e -> chemicalElementsSet.add(e));
+        Set<String> compoundsSet = new HashSet<>();
+        for (int i = 0; i < numberOfCompounds; i++) {
+            // Добавям уникалните елементи от всеки прочетен ред
+            Arrays.stream(scanner.nextLine().split("\\s+")).forEach(e -> compoundsSet.add(e));
 
         }
 
-        chemicalElementsSet.forEach(e -> System.out.print(e + " "));
+        compoundsSet.stream().sorted().forEach(e -> System.out.print(e + " ")); // <- По азбучен ред
 
     }
 
---
+++ SetsMaps/PeriodicTable-03/PeriodicTable_03_3.java
@@ -0,0 +1,28 @@
+package SetsMaps;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PeriodicTable_03_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int lines = Integer.parseInt(scanner.nextLine());
+
+        Set<String> chemicalElementsSet = new TreeSet<>(); // <- По азбучен ред
+
+        while (lines-- > 0) {
+
+            String[] currentElementsArr = scanner.nextLine().split("\\s+");
+            // Добавям текущите прочетени елементи
+            Arrays.stream(currentElementsArr).forEach(e -> chemicalElementsSet.add(e));
+
+        }
+
+        chemicalElementsSet.forEach(e -> System.out.print(e + " "));
+
+    }
+
+}
