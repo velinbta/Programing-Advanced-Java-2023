48daf793617ede60d1aa05c6be8140753ead6748
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-08 04:20:39 +0300

    Solution added

---
+++ StreamsFilesDirectories/WriteEveryThirdLine-05/WriteEveryThirdLine_05_1.java
@@ -0,0 +1,43 @@
+package StreamsFilesDirectories;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class WriteEveryThirdLine_05_1 {
+    public static void main(String[] args) {
+
+        String inPath = "StreamsFilesDirectories/WriteEveryThirdLine-05/input.txt";
+        String outPath = "StreamsFilesDirectories/WriteEveryThirdLine-05/WriteEveryThirdLine_05_1_output";
+
+        try (Scanner scanner = new Scanner(new FileInputStream(inPath));
+             PrintWriter out = new PrintWriter(new FileOutputStream(outPath))) {
+
+            int lineCounter = 1;
+            while (scanner.hasNextLine()) { // <- Докато има следващ ред
+
+                if (lineCounter % 3 == 0) {
+
+                    out.println(scanner.nextLine()); // <- Записвам всеки трети ред
+
+                } else {
+
+                    scanner.nextLine(); // <- Слизам на следващия
+
+                }
+
+                lineCounter++;
+
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("File not found, please review source!");
+
+        }
+
+    }
+
+}
---
+++ StreamsFilesDirectories/WriteEveryThirdLine-05/WriteEveryThirdLine_05_2.java
@@ -0,0 +1,41 @@
+package StreamsFilesDirectories;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteEveryThirdLine_05_2 {
+    public static void main(String[] args) {
+
+        String inPath = "StreamsFilesDirectories/WriteEveryThirdLine-05/input.txt";
+        String outPath = "StreamsFilesDirectories/WriteEveryThirdLine-05/WriteEveryThirdLine_05_2_output";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inPath));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outPath))) {
+
+            String line = reader.readLine();
+            int lineCounter = 1;
+
+            while (line != null) {
+
+                if (lineCounter % 3 == 0) {
+
+                    writer.write(line); // <- Записвам всеки трети ред
+                    writer.newLine(); // <- Слизам на следващия ред
+                }
+
+                lineCounter++;
+                line = reader.readLine(); // <- Прочитам следващия ред от текста
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("File not found, please review source!");
+
+        }
+
+    }
+
+}
