7c6d7ff077e27c4815853bc3f95b68a4deef323b
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-08 08:38:21 +0300

    Solution added

---
+++ StreamsFilesDirectories/ListFiles-07/ListFiles_07.java
@@ -0,0 +1,28 @@
+package StreamsFilesDirectories;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class ListFiles_07 {
+    public static void main(String[] args) {
+
+        String inPath = "StreamsFilesDirectories/ListFiles-07/Files-and-Streams";
+
+        try {
+
+            File file = new File(inPath);
+            File[] filesArr = file.listFiles(f -> f.isFile()); // <- Масив, само от файловете
+
+            Arrays.stream(filesArr).forEach(f ->
+                    System.out.printf("%s: [%d]\n", f.getName(), f.length())); // <- Име, големина
+
+        } catch (NullPointerException e) {
+
+            System.out.println("No files available"); // <- Ако не намери папки
+
+        }
+
+    }
+
+}
+
---
+++ StreamsFilesDirectories/ListFiles-07/README.txt
@@ -0,0 +1,15 @@
+7. List Files
+You are provided a folder named "Files-and-Streams". Create a program that lists the names and file sizes (in bytes) of all files that are placed directly in it (do not include files in nested folders). 
+
+Examples:
+
+Input: (Inside "Files-and-Streams"):
+Files-and-Streams
+Serialization
+Streams-and-Files
+input.txt
+output.txt
+
+Output:
+input.txt: [size in bytes]
+output.txt: [size in bytes]
