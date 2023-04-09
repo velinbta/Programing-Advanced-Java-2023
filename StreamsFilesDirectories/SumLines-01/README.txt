0ee937eced5c88242560f18a264414e90c39018c
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-09 05:28:27 +0300

    Solution added

---
+++ StreamsFilesDirectories/SumLines-01/README.txt
@@ -0,0 +1,22 @@
+1. Sum Lines
+Write a program that reads a text file ("input.txt" from the Resources - Exercises(find in repository)) and prints on the console the sum of the ASCII symbols of each of its lines. Use BufferedReader in combination with FileReader.
+
+Examples:
+
+Input:
+On January 1 , 1533 , 
+Michael Angelo, 
+then fifty-seven years old, 
+writes
+from Florence to 
+Tommaso de' Cavalieri, 
+a youth of noble Roman family,
+
+Output:
+1452
+1397
+2606
+670
+1573
+2028
+2762
---
+++ StreamsFilesDirectories/SumLines-01/SumLines_01_1.java
@@ -0,0 +1,54 @@
+package StreamsFilesDirectories;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumLines_01_1 {
+    public static void main(String[] args) {
+
+        String path = "StreamsFilesDirectories/SumLines-01/input.txt";
+
+        try {
+
+            BufferedReader in = new BufferedReader(new FileReader(path));
+
+            int oneByte = in.read(); // <- Байт по байт
+
+            List<Long> lineAsciiValuesList = new ArrayList<>();
+            long count = 0;
+            while (oneByte != -1) {
+
+                if (oneByte == '\r') { // <- Carriage return, прочитам нов байт
+
+                    oneByte = in.read();
+
+                } else if (oneByte == '\n') { // <- На новия ред добавям към списъка и нулирам брояча
+
+                    lineAsciiValuesList.add(count);
+                    count = 0;
+                    oneByte = in.read();
+
+                } else { // <- Броя байтовете на реда
+
+                    count += oneByte;
+                    oneByte = in.read();
+
+                }
+
+            }
+
+            lineAsciiValuesList.add(count); // <- Добавям последните преброени бойтове на реда
+            lineAsciiValuesList.forEach(n -> System.out.println(n));
+
+        } catch (IOException e) {
+
+            System.out.println("File not found,please review source!");
+
+        }
+
+    }
+
+}
