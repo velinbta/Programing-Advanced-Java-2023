f389c05cfd2f0152f60b199c59e8a2058a9c1a9c
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-12 13:31:41 +0300

    Solution added

---
+++ FunctionalProgramming/CountUppercaseWords-03/CountUppercaseWords_03.java
@@ -0,0 +1,22 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class CountUppercaseWords_03 {
+    public static void main(String[] args) {
+
+        String line = new Scanner(System.in).nextLine();
+
+        Predicate<String> isUpperCase = w -> Character.isUpperCase(w.charAt(0));
+
+        String[] upperCaseWords = Arrays.stream(line.split("\\s+")).filter(isUpperCase)
+                .toArray(String[]::new); // <- Само думите с главни букви
+
+        System.out.println(upperCaseWords.length);
+        System.out.println(String.join(System.lineSeparator(), upperCaseWords));
+
+    }
+
+}
---
+++ FunctionalProgramming/CountUppercaseWords-03/README.txt
@@ -0,0 +1,22 @@
+3. Count Uppercase Words
+Write a program that reads one line of text from the console. Print the count of words that start with an Uppercase letter, after that print all these words in the same order as you found them in the text.
+Use a Predicate<String>.
+
+Examples:
+
+Input 1:
+The following example shows how to use Predicate
+
+Output 1:
+2
+The
+Predicate
+
+Input 2:
+Write a program that reads one line of text from console. Print count of words that start with Uppercase, after that print all those words in the same order as you find them in text.
+
+Output 2:
+3
+Write
+Print
+Uppercase,
