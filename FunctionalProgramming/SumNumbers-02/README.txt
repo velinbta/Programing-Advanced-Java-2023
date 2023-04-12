cb602c0b415e94f4363172ea97c618e1521d6110
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-12 11:46:36 +0300

    Solution added

---
+++ FunctionalProgramming/SumNumbers-02/README.txt
@@ -0,0 +1,19 @@
+2. Sum Numbers
+Write a program that reads one line of Integers separated by ", ". Print the count of the numbers and their sum.
+Use a Function<String, Integer>.
+
+Examples:
+
+Input 1:
+4, 2, 1, 3, 5, 7, 1, 4, 2, 12
+
+Output 1:
+Count = 10
+Sum = 41
+
+Input 2:
+2, 4, 6
+
+Output 2:
+Count = 3
+Sum = 12
---
+++ FunctionalProgramming/SumNumbers-02/SumNumbers_02.java
@@ -0,0 +1,23 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class SumNumbers_02 {
+    public static void main(String[] args) {
+
+        String input = new Scanner(System.in).nextLine();
+
+        Function<String, Integer> count = n -> Arrays.stream(input.split(",\\s+"))
+                .mapToInt(Integer::parseInt).toArray().length; // <- Брой числа
+
+        Function<String, Integer> sum = n -> Arrays.stream(input.split(",\\s+"))
+                .mapToInt(Integer::parseInt).sum(); // <- Сума числа
+
+        System.out.printf("Count = %d\n", count.apply(input));
+        System.out.printf("Sum = %d\n", sum.apply(input));
+
+    }
+
+}
