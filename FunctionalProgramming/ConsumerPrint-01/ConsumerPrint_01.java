cb17689d23e257f10bac0054a336067af352c8a3
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-13 21:33:30 +0300

    Solution added

---
+++ FunctionalProgramming/ConsumerPrint-01/ConsumerPrint_01.java
@@ -0,0 +1,18 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class ConsumerPrint_01 {
+    public static void main(String[] args) {
+
+        String[] words = new Scanner(System.in).nextLine().split("\\s+");
+
+        Consumer<String[]> printer = n -> Arrays.stream(n).forEach(System.out::println);
+
+        printer.accept(words);
+
+    }
+
+}
---
+++ FunctionalProgramming/ConsumerPrint-01/README.txt
@@ -0,0 +1,20 @@
+1. Consumer Print
+Write a program that reads a collection of strings, separated by one or more whitespaces, from the console and then prints them onto the console. Each string should be printed on a new line. Use a Consumer<T>.
+
+Examples:
+
+Input 1:
+Peter George Alex
+
+Output 1:
+Peter
+George
+Alex
+
+Input 2:
+John Sam Sara
+
+Output 2:
+John
+Sam
+Sara
