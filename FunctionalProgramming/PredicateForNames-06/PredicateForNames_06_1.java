2c141bcf79c8b2e53d46d23bde4801e69b875f2b
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-16 04:05:07 +0300

    Solution added

---
+++ FunctionalProgramming/PredicateForNames-06/PredicateForNames_06_1.java
@@ -0,0 +1,21 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class PredicateForNames_06_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int length = Integer.parseInt(scanner.nextLine());
+        String[] names = scanner.nextLine().split("\\s+");
+
+        // По-малка или равна дължина от даденото число
+        Predicate<String> lengthEqualOrLess = n -> n.length() <= length;
+
+        Arrays.stream(names).filter(lengthEqualOrLess).forEach(System.out::println);
+
+    }
+
+}
