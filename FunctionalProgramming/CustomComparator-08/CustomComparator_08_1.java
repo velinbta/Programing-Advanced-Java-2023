fa77b38e89d5134451b4ac28a5b1639fae687a16
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-18 06:22:42 +0300

    Solution added

---
+++ FunctionalProgramming/CustomComparator-08/CustomComparator_08_1.java
@@ -0,0 +1,24 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class CustomComparator_08_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Integer[] naturalOrderSorted = Arrays.stream(input.split("\\s+")).map(Integer::parseInt)
+                .sorted().toArray(Integer[]::new);
+
+        Function<Integer, Boolean> evenFirst = n -> n % 2 != 0; // <- Първо четните числа, след това нечетните
+
+        Arrays.stream(naturalOrderSorted).sorted(Comparator.comparing(evenFirst))
+                .forEach(n -> System.out.print(n + " ")); // <- Сортирам и принтирам
+
+    }
+
+}
---
+++ FunctionalProgramming/CustomComparator-08/README.txt
@@ -0,0 +1,22 @@
+8. Custom Comparator
+Write a custom comparator that sorts all even numbers before all odd ones in ascending order. Pass it to an Arrays.sort() function and print the result.
+
+Examples:
+
+Input 1:
+1 2 3 4 5 6
+
+Output 1:
+2 4 6 1 3 5
+
+Input 2:
+-3 2
+
+Output 2:
+2 -3
+
+Input 3:
+-111 2 333 444 555 22 73 69 13
+
+Output 3:
+2 22 444 -111 13 69 73 333 555
