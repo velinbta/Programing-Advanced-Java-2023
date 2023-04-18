cb7ff0985a672e7f398009f2646a8dfc66d8adfb
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-18 05:16:54 +0300

    Solution added

---
+++ FunctionalProgramming/ListOfPredicates-09/ListOfPredicates_09.java
@@ -0,0 +1,26 @@
+package FunctionalProgramming;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ListOfPredicates_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int upperBound = Integer.parseInt(scanner.nextLine());
+
+        Set<Integer> divisibleSet = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toSet());
+
+        List<Integer> allNumbersSet = IntStream.rangeClosed(1, upperBound).boxed()
+                .collect(Collectors.toList()); // <- От 1 до горната граница включително
+
+        // Премахвам всички, които не се делят на числата от divisibleSet
+        divisibleSet.forEach(division -> allNumbersSet.removeIf(n -> n % division != 0));
+
+        allNumbersSet.forEach(n -> System.out.print(n + " "));
+
+    }
+
+}
---
+++ FunctionalProgramming/ListOfPredicates-09/README.txt
@@ -0,0 +1,18 @@
+9. List of Predicates
+Find all numbers in the range 1...N that is divisible by the numbers of a given sequence. Use predicates.
+
+Examples:
+
+Input 1:
+10
+1 1 1 2
+
+Output 1:
+2 4 6 8 10
+
+Input 2:
+100
+2 5 10 20
+
+Output 2:
+20 40 60 80 100
