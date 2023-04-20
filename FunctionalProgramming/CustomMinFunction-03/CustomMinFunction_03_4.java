e4b433b937e5162e9dd667b5d102ce3cdd6daac7
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-20 03:55:09 +0300

    Solution added

--- FunctionalProgramming/CustomMinFunction-03/CustomMinFunction_03_4.java
+++ FunctionalProgramming/CustomMinFunction-03/CustomMinFunction_03_4.java
@@ -1,13 +1,21 @@
 package FunctionalProgramming;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CustomMinFunction_03_4 {
     public static void main(String[] args) {
 
-        int min = Arrays.stream(new Scanner(System.in).nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt).min().orElse(-1); // <- Най-малкото число на прочетения ред
+        String numbersAsString = new Scanner(System.in).nextLine();
+
+        // Превръщам в колекция и намирам най-малкото число
+        Function<String, Integer> minFunc = s -> Collections.min(Arrays.stream(s.split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList()));
+
+        int min = minFunc.apply(numbersAsString);
 
         System.out.println(min);
 
---
+++ FunctionalProgramming/CustomMinFunction-03/CustomMinFunction_03_5.java
@@ -0,0 +1,16 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CustomMinFunction_03_5 {
+    public static void main(String[] args) {
+
+        int min = Arrays.stream(new Scanner(System.in).nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).min().orElse(-1); // <- Най-малкото число на прочетения ред
+
+        System.out.println(min);
+
+    }
+
+}
