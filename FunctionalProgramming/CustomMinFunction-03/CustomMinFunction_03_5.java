f9bc5729245bbd121ee4cc3c71e96a3715f04cfa
velinbta <122949696+velinbta@users.noreply.github.com>
2023-04-20 04:15:05 +0300

    Solution added

--- FunctionalProgramming/CustomMinFunction-03/CustomMinFunction_03_5.java
+++ FunctionalProgramming/CustomMinFunction-03/CustomMinFunction_03_5.java
@@ -2,20 +2,16 @@ package FunctionalProgramming;
 
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.function.UnaryOperator;
+import java.util.function.Consumer;
 
 public class CustomMinFunction_03_5 {
     public static void main(String[] args) {
 
-        String numbersAsString = new Scanner(System.in).nextLine();
+        // Намира най-малкото число от прочетен String, и го принтира
+        Consumer<String> readLineOfStringFindMinAndPrint = line -> System.out.println
+                (Arrays.stream(line.split("\\s+")).mapToInt(Integer::parseInt).min().orElse(-1));
 
-        // Намирам най-малкото число от прочетения ред и го връщам като String
-        UnaryOperator<String> minStringOperator = s -> String.valueOf(Arrays.stream(s.split("\\s+"))
-                .mapToInt(Integer::parseInt).min().orElse(-1));
-
-        String min = minStringOperator.apply(numbersAsString);
-
-        System.out.println(min);
+        readLineOfStringFindMinAndPrint.accept(new Scanner(System.in).nextLine());
 
     }
 
--- FunctionalProgramming/CustomMinFunction-03/CustomMinFunction_03_6.java
+++ FunctionalProgramming/CustomMinFunction-03/CustomMinFunction_03_6.java
@@ -2,12 +2,18 @@ package FunctionalProgramming;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.UnaryOperator;
 
 public class CustomMinFunction_03_6 {
     public static void main(String[] args) {
 
-        int min = Arrays.stream(new Scanner(System.in).nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt).min().orElse(-1); // <- Най-малкото число на прочетения ред
+        String numbersAsString = new Scanner(System.in).nextLine();
+
+        // Намирам най-малкото число от прочетения ред и го връщам като String
+        UnaryOperator<String> minStringOperator = s -> String.valueOf(Arrays.stream(s.split("\\s+"))
+                .mapToInt(Integer::parseInt).min().orElse(-1));
+
+        String min = minStringOperator.apply(numbersAsString);
 
         System.out.println(min);
 
