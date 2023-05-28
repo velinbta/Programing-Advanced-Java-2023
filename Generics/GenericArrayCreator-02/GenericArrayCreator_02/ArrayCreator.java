2fdc0ac1fc96d0e452a53aa0daa364b57dfb7dcd
velinbta <122949696+velinbta@users.noreply.github.com>
2023-05-29 00:04:07 +0300

    Updated

--- Generics/GenericArrayCreator-02/GenericArrayCreator_02/ArrayCreator.java
+++ Generics/GenericArrayCreator-02/GenericArrayCreator_02/ArrayCreator.java
@@ -8,6 +8,8 @@ public class ArrayCreator {
     public static <T> T[] create(int length, T defValue) {
 
         Class<?> clazz = defValue.getClass(); // <- От тук реферира за тип
+
+        @SuppressWarnings("unchecked")
         T[] array = (T[]) Array.newInstance(clazz, length); // <- Type cast
 
         Arrays.fill(array, defValue);
@@ -16,7 +18,8 @@ public class ArrayCreator {
     }
 
     public static <T> T[] create(Class<T> clazz, int length, T defValue) {
-
+        
+        @SuppressWarnings("unchecked")
         T[] array = (T[]) Array.newInstance(clazz, length); // <- Типът, се подава при аргументите
         Arrays.fill(array, defValue);
 
