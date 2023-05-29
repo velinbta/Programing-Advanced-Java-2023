9e15cfe0bdbd1089481c3a551e0d136dba9be432
velinbta <122949696+velinbta@users.noreply.github.com>
2023-05-30 01:15:28 +0300

    Updated

--- Generics/GenericArrayCreator-02/GenericArrayCreator_02/Main.java
+++ Generics/GenericArrayCreator-02/GenericArrayCreator_02/Main.java
@@ -2,6 +2,7 @@ package GenericArrayCreator_02;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +12,11 @@ public class Main {
         Double[] doubles = ArrayCreator.create(Double.class, 5, 13.00);
         Scanner[] scanners = ArrayCreator.create(Scanner.class, 3, new Scanner(System.in));
 
-        Arrays.stream(doubles).forEach(d -> System.out.printf("%.2f ", d));
+        System.out.println(Arrays.stream(strings).collect(Collectors.joining(", ")));
+        System.out.println(Arrays.stream(integers).map(String::valueOf).collect(Collectors.joining(", ")));
+        System.out.println(Arrays.stream(doubles).map(String::valueOf)
+                .collect(Collectors.joining(", ")).replaceAll(".0", ".00"));
+        System.out.printf("Scanner[] contains %d scanners\n", scanners.length);
 
     }
 
--- Generics/JarOfT-01/README.txt
+++ Generics/JarOfT-01/README.txt
@@ -5,3 +5,12 @@ It should have two public methods:
 void add(element)
 element remove()
 Adding should add on top of its contents. Remove should get the topmost element.
+
+Examples:
+
+Jar<Pickle> jarOfPickles = new Jar<>();
+
+jarOfPickles.add(new Pickle());
+jarOfPickles.add(new Pickle());
+
+Pickle pickle = jarOfPickles.remove();
