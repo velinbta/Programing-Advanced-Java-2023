c9303b434b111819d4056b7a7eebfd120466c20f
velinbta <122949696+velinbta@users.noreply.github.com>
2023-06-03 19:33:19 +0300

    Solution added

---
+++ Generics/CustomList-07/CustomList_07_1/Main.java
@@ -0,0 +1,89 @@
+package CustomList_07_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        CustomList<String> list = new CustomList<>();
+
+        while (!input.equals("END")) {
+
+            String[] data = input.split("\\s+");
+            String command = data[0];
+
+            switch (command) {
+
+                case "Add": {
+
+                    String element = data[1];
+                    list.add(element);
+
+                }
+                break;
+
+                case "Remove": {
+
+                    int index = Integer.parseInt(data[1]);
+                    list.remove(index);
+
+                }
+                break;
+
+                case "Contains": {
+
+                    String element = data[1];
+                    System.out.println(list.contains(element));
+
+                }
+                break;
+
+                case "Swap": { // <- Разменя местата на два индекса в списъка
+
+                    int firstIndex = Integer.parseInt(data[1]);
+                    int secondIndex = Integer.parseInt(data[2]);
+                    list.swap(firstIndex, secondIndex);
+
+                }
+                break;
+
+                case "Greater": { // <- Принтира, колко са по-големи от дадения елемент
+
+                    String element = data[1];
+                    System.out.println(list.countGreaterThan(element));
+
+                }
+                break;
+
+                case "Max": // <- Най-голямото
+
+                    System.out.println(list.getMax());
+
+                    break;
+
+                case "Min": // <- Най-малкото
+
+                    System.out.println(list.getMin());
+
+                    break;
+
+                case "Print":
+
+                    System.out.println(list);
+
+                    break;
+
+                default:
+                    throw new IllegalArgumentException(String.format("Invalid command \"%s\"", command));
+
+            }
+
+            input = scanner.nextLine();
+        }
+
+    }
+
+}
---
+++ Generics/CustomList-07/README.txt
@@ -0,0 +1,60 @@
+7. Custom List
+Create a generic data structure that can store any type that can be compared. Implement functions:
+void add(T element)
+T remove(int index)
+boolean contains(T element)
+void swap(int index, int index)
+int countGreaterThan(T element)
+T getMax()
+T getMin()
+
+Create a command interpreter that reads commands and modifies the custom list that you have created. Implement the commands:
+Add {element} - Adds the given element to the end of the list.
+Remove {index} - Removes the element at the given index.
+Contains {element} - Prints if the list contains the given element (true or false).
+Swap {index1} {index2} - Swaps the elements at the given indexes.
+Greater {element} - Counts the elements that are greater than the given element and prints their count
+Max - Prints the maximum element in the list.
+Min - Prints the minimum element in the list.
+Print - Prints all elements in the list, each on a separate line.
+END - stops the reading of commands.
+
+Note: For the Judge tests, use String as T.
+
+Examples:
+
+Input 1:
+Add aa
+Add bb
+Add cc
+Max
+Min
+Greater aa
+Swap 0 2
+Contains aa
+Print
+END
+
+Output 1:
+cc
+aa
+2
+true
+cc
+bb
+aa
+
+Input 2:
+Add Peter
+Add George
+Swap 0 0
+Swap 1 1
+Swap 0 1
+Swap 1 0
+Swap 0 1
+Print
+END
+
+Output 2:
+George
+Peter
